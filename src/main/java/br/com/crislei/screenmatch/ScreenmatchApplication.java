01855727540fcc79d0bba6be72f3603e5520d7b7
Crislei Silvagit config --global user.email crisleisilva1905@gmail.com.gitconfiggit config --listgit config --global user.name Crislei <crisleisilva1905@gmail.com>
2024-05-03 18:11:46 -0300

    Inicio do Projeto Spring

---
+++ src/main/java/br/com/crislei/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,28 @@
+package br.com.crislei.screenmatch;
+
+import br.com.crislei.screenmatch.model.DadosSerie;
+import br.com.crislei.screenmatch.service.ConsumoAPI;
+import br.com.crislei.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=This+is+us&apikey=c6689f79");
+	//	System.out.println(json);
+	//	json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
